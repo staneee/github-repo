eff63f95e99286553253891f25e1da328357e2b9
stan e <yi.hang@live.com>
2024-04-21 20:45:48 +0800

    测试依赖

---
+++ build/clone-target-repo.ps1
@@ -0,0 +1,38 @@
+# 仓库地址
+$RepoUrl = $env:GIT_REPO
+
+# 顶级目录
+$rootPath = Split-Path -Parent (Get-Location).Path
+
+# 读取当前项目配置
+$ciConfigPath = Join-Path $rootPath "src" "ci-config.json"
+$ciConfig = (Get-Content -Path $ciConfigPath -Encoding UTF8) | ConvertFrom-Json
+
+# 克隆目标仓库代码
+## git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}
+if($ciConfig.mode -eq 'commit'){
+    git clone --depth 1 `
+        -b $ciConfig.git.branch `
+        $RepoUrl repo-code
+    git log
+    git checkout $ciConfig.git.commit
+}
+if($ciConfig.mode -eq 'tag'){
+    git clone --depth 1 `
+        --branch $ciConfig.git.tag `
+        $RepoUrl repo-code
+}
+
+# 切换到目标仓库代码
+# Set-Location repo-code
+
+
+
+
+
+
+
+
+
+
+
